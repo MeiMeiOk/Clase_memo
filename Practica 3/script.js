a64d43fc35cb1079f4e614105a27e5e91a4bde41
MeiMei <memomem1nmem1tome1me1@gmail.com>
2026-02-16 07:50:34 -0600

    Add files via upload

---
+++ Practica 3/script.js
@@ -0,0 +1,32 @@
+function alertPer(){
+    alert('PERDISTE BRO...');
+}
+
+
+function rever(img){
+    img.src = "img/rever.png"
+}
+
+let id1 = null;
+let id2 = null;
+
+function cartaClick(id, idcelda){
+    
+    if(id1 == 0){
+    id1 = id;
+
+    id2 = idcelda;
+    }
+
+    else{
+        if (id1 == id) {
+            alert("Pareja encontrada :)")
+        }else{
+            alert
+        }
+    }
+
+
+    
+}
+
